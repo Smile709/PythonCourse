3e6216b878975847de4b193f42e2cff318712511
Ivan Murashov <smile709@ya.com>
2023-10-22 20:23:47 +0300

    update sem3_home1

--- HomeWork/sem3_home1.py
+++ HomeWork/sem3_home1.py
@@ -5,10 +5,10 @@
 k = 3
 # 1
 '''
-list_1 = [1,2,3,4,5]
+list_1 = [1,2,3,4,5,3,6,8]
 k=3
 count=0
-for i in range(len(list_1)):
+for i in list_1:
     if i==k:
         count+=1
 print(f"число {k} встречается {count} раз")
