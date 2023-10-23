5bd108bbcb1dc3a46534761a0dbdcca4c865b9be
Ivan Murashov <smile709@ya.com>
2023-10-23 11:48:34 +0300

    add task27

--- Seminars/task27.py
+++ Seminars/task27.py
@@ -5,3 +5,8 @@ Input: She sells sea shells on the sea shore The shells that she sells are sea s
 I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
 Output: 13
 '''
+input_str = '''She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure.So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells'''.replace("\'",' ').replace(".",' ').upper().split()
+print(input_str,len(set(input_str)))
