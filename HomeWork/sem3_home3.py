ea2cc782879c51f30539c9ef643e6c8fb51f922b
Ivan Murashov <smile709@ya.com>
2023-10-23 10:35:31 +0300

    update sem3_home3

--- HomeWork/sem3_home3.py
+++ HomeWork/sem3_home3.py
@@ -24,34 +24,50 @@ k = 'ноутбук'
 # 12
 '''
 
-englishLetter ={
-                'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'S': 1, 'T': 1, 'R': 1,
-                'D': 2, 'G': 2,
-                'B': 3, 'C': 3, 'M': 3, 'P': 3,
-                'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-                'K': 5,
-                'J': 6, 'X': 8,
-                'Q': 10, 'Z': 10
-                }
+# englishLetter ={
+#                 'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'S': 1, 'T': 1, 'R': 1,
+#                 'D': 2, 'G': 2,
+#                 'B': 3, 'C': 3, 'M': 3, 'P': 3,
+#                 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+#                 'K': 5,
+#                 'J': 6, 'X': 8,
+#                 'Q': 10, 'Z': 10
+#                 }
 
-russianLetter ={
-                'А': 1, 'В': 1, 'Е': 1, 'И': 1, 'Н': 1, 'О': 1, 'Р': 1, 'С': 1, 'Т': 1,
-                'Д': 2, 'К': 2, 'Л': 2, 'М': 2, 'П': 2, 'У': 2,
-                'Б': 3, 'Г': 3, 'Ё': 3, 'Ь': 3, 'Я': 3,
-                'Й': 4, 'Ы': 4,
-                'Ж': 5, 'З': 5, 'Х': 5, 'Ц': 5, 'Ч': 5,
-                'Ш': 8, 'Э': 8, 'Ю': 8,
-                'Ф': 10, 'Щ': 10, 'Ъ':10
-                }
+# russianLetter ={
+#                 'А': 1, 'В': 1, 'Е': 1, 'И': 1, 'Н': 1, 'О': 1, 'Р': 1, 'С': 1, 'Т': 1,
+#                 'Д': 2, 'К': 2, 'Л': 2, 'М': 2, 'П': 2, 'У': 2,
+#                 'Б': 3, 'Г': 3, 'Ё': 3, 'Ь': 3, 'Я': 3,
+#                 'Й': 4, 'Ы': 4,
+#                 'Ж': 5, 'З': 5, 'Х': 5, 'Ц': 5, 'Ч': 5,
+#                 'Ш': 8, 'Э': 8, 'Ю': 8,
+#                 'Ф': 10, 'Щ': 10, 'Ъ':10
+#                 }
 
-k = 'ноутбук'
+# englishLetter = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# russianLetter = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
 
-score=0
+# k = 'ноутбук'
 
-for letter in k.upper():
-    if letter in englishLetter:
-        score += englishLetter[letter]
-    elif letter in russianLetter:
-        score += russianLetter[letter]
+# score=0
 
-print(score)
+# for letter in k.upper():
+#     if letter in englishLetter:
+#         score += englishLetter[letter]
+#     elif letter in russianLetter:
+#         score += russianLetter[letter]
+
+# print(score)
+
+points_en = {'AEIOULNSTR': 2 ,'DG':3, 'BCMP': 4,'y'=1, 'g'=4}
+
+word = input("input name ").upper()  # переводим все буквы в верхний регистр
+count = 0
+
+for i in word:
+    
+    for j in points_en:
+        if i in j:
+            count = count + points_en[j]
+  
+print(count)
