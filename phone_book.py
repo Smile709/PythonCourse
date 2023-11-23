e3c28aba439bf57063d2dd1b46d5f03bd6e9e625
Ivan Murashov <smile709@ya.com>
2023-11-23 18:27:19 +0300

    update Phone_book

--- phone_book.py
+++ phone_book.py
@@ -33,15 +33,14 @@ def work_with_phonebook(filename): # Бот программы (основная
             number=input('Номер тел.: ')
             print(find_by_number(phone_book, number))
         elif choice==6:
-    #        user_line=input('Новая запись: ')
+            user_line=input('Новая запись(Фамилия, Имя, Телефон, Описание): ')
             add_user(phone_book, user_line)
-    #        write_txt(filename, phone_book)
         elif choice==7:
             write_txt(filename, phone_book)
 
         choice=show_menu()
 
-def show_menu():
+def show_menu(): # Печатает меню выбора
     print('1. Распечатать справочник', 
           '2. Найти телефон по фамилии', 
           '3. Изменить номер телефона', 
@@ -58,53 +57,51 @@ def read_txt(filename): #Добавить текст в программу
     fields=['Фамилия', 'Имя', 'Телефон', 'Описание']
     with open(filename,'r', encoding='utf-8') as file:
         for line in file:
-            record = dict(zip(fields, line.split(',')))    
+            record = dict(zip(fields, line.split(',')))
+            if '\n' in record['Описание']:
+                record['Описание'] = record['Описание'][:-1]
             phone_book.append(record)
     return phone_book
 
-def write_txt(filename, phone_book): #Записать программу в файл
+def write_txt(filename, phone_book): #7.Записать программу в файл
     with open(filename,'w',encoding='utf-8') as file:
         for i in range(len(phone_book)):
             s='' 
             for v in phone_book[i].values():
                 s+=v+','
             file.write(f'{s[:-1]}\n')
-
-def print_result(phone_book): #Распечатать справочник в консоль
+    print('Изменения внесены в файл')
+def print_result(phone_book): #1.Распечатать справочник в консоль
     if len(phone_book) > 0:
         for line in phone_book:
-            print(f"{line['Фамилия']}, {line['Имя']}, {line['Телефон']}, {line['Описание']}")
+            print(f"{line['Фамилия'].ljust(20)}{line['Имя'].ljust(20)}{line['Телефон'].ljust(20)}{line['Описание'].ljust(20)}")
     else: print(f"Файл не существует.")
 
-def find_by_lastname(phone_book, last_name):
+def find_by_lastname(phone_book, last_name): #2.Поиск по фамилии
     for line in phone_book:
         if line['Фамилия'].lower() == last_name.lower():
-            result = line['Телефон'] 
-        else: result = "Такой Фамилии нет"
-    return result
+            return line
 
-def find_by_number(phone_book, number):
-    for line in phone_book:
-        if line['Телефон'].lower() == number.lower():
-            result = line['Фамилия', 'Имя'] 
-        else: result = "Такого номера нет"
-    return result
-
-def change_number(phone_book, last_name, new_number):
+def change_number(phone_book, last_name, new_number): #3.Изменить номер тел.
     for line in phone_book:
         if line['Фамилия'].lower() == last_name.lower():
-            line['Телефон'] = new_number
-        else: result = "Такой Фамилии нет"
-    return result
+            line['Телефон'] = str(new_number)
+    return phone_book
 
-def delete_by_lastname(phone_book, last_name):
+def delete_by_lastname(phone_book, last_name): #4.Удалить запись
+    count=0
     for line in phone_book:
+        count+=1
         if line['Фамилия'].lower() == last_name.lower():
-            line[:] = []
-        else: result = "Такой Фамилии нет"
-    return result
+            phone_book.pop(count-1)
+    return phone_book
 
-def add_user(phone_book, user_line):
+def find_by_number(phone_book, number): #5.Поиск по номеру тел.
+    for line in phone_book:
+        if line['Телефон'] == str(number):
+            return line
+
+def add_user(phone_book, user_line): #6.Добавить запись
     fields=['Фамилия', 'Имя', 'Телефон', 'Описание']
     record = dict(zip(fields, user_line.split(',')))
     phone_book.append(record)
